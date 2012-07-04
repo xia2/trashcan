03bce1f392088a5322a39f2d81e31b50d23af7b4
Graeme Winter <graeme.winter@gmail.com>
2012-07-04 13:58:24 +0000

    Histogram of cc's

---
+++ xia2-talks/talks/aca-chef/figures/gold/shelxd_cc_a.sh
@@ -0,0 +1,15 @@
+gnuplot << eof
+set terminal epslatex size 4,2.82 standalone color colortext 9
+set output 'shelxd_cc_a.tex'
+set xrange [0:40]
+set yrange [0:200]
+set xlabel 'CC/\%'
+set ylabel 'ntry / 200'
+plot '900_shelxd_cc_a.dat' using 1:2 with lines title '900 frames', \
+'900_shelxd_cc_a.dat' using 1:3 with lines title '900 frames (weak)', \
+'1800_shelxd_cc_a.dat' using 1:2 with lines title '1800 frames', \
+'1800_shelxd_cc_a.dat' using 1:3 with lines title '1800 frames (weak)'
+eof
+
+pdflatex shelxd_cc_a.tex
+
