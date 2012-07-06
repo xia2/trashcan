7e1ae631b01b1608b1af5f1dbf588face46f811a
Graeme Winter <graeme.winter@gmail.com>
2012-07-06 14:33:41 +0000

    Added in Rcp plots

--- xia2-talks/talks/aca-chef/figures/simple2/comp.sh
+++ xia2-talks/talks/aca-chef/figures/simple2/comp.sh
@@ -1,8 +1,10 @@
 gnuplot << eof
 set terminal epslatex size 4,2.82 standalone color colortext 9
 set output 'comp.tex'
-set xlabel 'Image number'
+set xlabel 'Total number of frames'
 set ylabel 'Completeness'
+set xrange [0:900]
+set yrange [0:1.05]
 plot 'comp.dat' using 1:2 with lines notitle
 eof
 
---
+++ xia2-talks/talks/aca-chef/figures/simple2/rcp.sh
@@ -0,0 +1,34 @@
+gnuplot << eof
+set terminal epslatex size 4,2.82 standalone color colortext 9
+set output 'rcp.tex'
+set xlabel 'Total number of frames'
+set ylabel 'Rcp'
+set xrange [0:900] 
+set yrange [0:0.06]
+plot 'rcp.dat' using 1:10 with lines notitle
+eof
+
+cat rcp.tex | python ../fix.py > rcp.tmp
+mv rcp.tmp rcp.tex
+
+pdflatex rcp.tex
+
+gnuplot << eof
+set terminal epslatex size 4,2.82 standalone color colortext 9
+set output 'rcp2.tex'
+set xlabel 'Total number of frames'
+set ylabel 'Rcp'
+set yrange [0:0.06]
+set y2range [0:1.05]
+set ytics nomirror
+set y2tics nomirror
+set key right bottom
+plot 'rcp.dat' using 1:10 with lines title 'Rcp', \
+'comp.dat' using 1:2 with lines title 'Completeness' axis x1y2
+eof
+
+cat rcp2.tex | python ../fix.py > rcp2.tmp
+mv rcp2.tmp rcp2.tex
+
+pdflatex rcp2.tex
+
