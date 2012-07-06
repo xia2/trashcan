c2fb41dd735631e716f309893b95e0b848ee57a3
Graeme Winter <graeme.winter@gmail.com>
2012-07-06 15:18:13 +0000

    Chef calculations

---
+++ xia2-talks/talks/aca-chef/figures/2isb/rcp.sh
@@ -0,0 +1,22 @@
+gnuplot << eof
+set terminal epslatex size 4,2.82 standalone color colortext 9
+set output 'rcp2.tex'
+set xlabel 'Experiment time / s'
+set ylabel 'Rcp'
+set xrange [0:1350]
+set yrange [0:0.08]
+set y2range [0:1.05]
+set ytics nomirror
+set y2tics nomirror
+set key right bottom
+plot 'rcp.dat' using 1:10 with lines title 'Rcp', \
+'comp1d.dat' using 1:2 with lines title 'Inflection' axis x1y2, \
+'comp2d.dat' using 1:2 with lines title 'Low remote' axis x1y2, \
+'comp3d.dat' using 1:2 with lines title 'Peak' axis x1y2
+eof
+
+cat rcp2.tex | python ../fix.py > rcp2.tmp
+mv rcp2.tmp rcp2.tex
+
+pdflatex rcp2.tex
+
