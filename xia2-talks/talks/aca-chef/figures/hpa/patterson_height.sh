c734b498f3e3f2677762873ba8ed698039d5c6cb
Graeme Winter <graeme.winter@gmail.com>
2012-07-06 10:57:56 +0000

    A nice plot (following a lot of processing effort) showing patterson peak height

---
+++ xia2-talks/talks/aca-chef/figures/hpa/patterson_height.sh
@@ -0,0 +1,14 @@
+gnuplot << eof
+set terminal epslatex size 4,2.82 standalone color colortext 9
+set output 'patterson_height.tex'
+set xlabel 'Extent of data set / $^{\circ}$'
+set ylabel 'Peak height / $\sigma$'
+set xrange [0:1440] 
+set yrange [0:20]
+plot 'patterson_height.dat' using 1:2 with lines notitle
+eof
+
+pdflatex patterson_height.tex
+
+
+
