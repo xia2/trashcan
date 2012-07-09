143a3cc8b7c05b098adfae33499c12ab8e1cf3d8
Graeme Winter <graeme.winter@gmail.com>
2012-07-09 08:46:21 +0000

    Added rcp plots

---
+++ xia2-talks/talks/aca-chef/figures/hpa/rcp.sh
@@ -0,0 +1,21 @@
+gnuplot << eof
+set terminal epslatex size 4,2.82 standalone color colortext 9
+set output 'rcp2.tex'
+set xlabel 'Total number of frames'
+set ylabel 'Rcp'
+set yrange [0:0.08]
+set y2range [0:1.05]
+set xrange [0:1440]
+set ytics nomirror
+set y2tics nomirror
+set key right bottom
+plot 'rcp.dat' using 1:10 with lines title 'Rcp', \
+'comp.dat' using 1:2 with lines title 'Completeness' axis x1y2, \
+'comp.dat' using 1:5 with lines title 'Anomalous' axis x1y2
+eof
+
+cat rcp2.tex | python ../fix.py > rcp2.tmp
+mv rcp2.tmp rcp2.tex
+
+pdflatex rcp2.tex
+
