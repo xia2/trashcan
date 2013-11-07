ba3ea8a71ff227bd187cb3aa5dcf46665464a890
Graeme Winter <graeme.winter@gmail.com>
2013-11-07 10:56:34 +0000

    Testing twospacifying

--- xia2-playpen/ward_cluster.py
+++ xia2-playpen/ward_cluster.py
@@ -1,109 +1,109 @@
 import copy
 
 def ward_cluster(data, distances = None):
-    '''Another go at ward clustering. Much of this depends on the fact that we
-    merge downwards.'''
+  '''Another go at ward clustering. Much of this depends on the fact that we
+  merge downwards.'''
 
-    n_data = len(data)
-    clusters = [[j] for j in range(n_data)]
-    indices = [j for j in range(len(clusters))]
+  n_data = len(data)
+  clusters = [[j] for j in range(n_data)]
+  indices = [j for j in range(len(clusters))]
 
-    history = []
+  history = []
 
-    # if we have not already been given computed distances, compute them,
-    # based on common sense...
+  # if we have not already been given computed distances, compute them,
+  # based on common sense...
 
-    if not distances:
-        distances = { }
-        for i in range(n_data - 1):
-            for j in range(i + 1, n_data):
-                try:
-                    distance = sum([(di - dj) * (di - dj) for \
-                                    di, dj in zip(data[i], data[j])])
-                except TypeError, e:
-                    distance = (data[i] - data[j]) * (data[i] - data[j])
+  if not distances:
+    distances = { }
+    for i in range(n_data - 1):
+      for j in range(i + 1, n_data):
+        try:
+          distance = sum([(di - dj) * (di - dj) for \
+                          di, dj in zip(data[i], data[j])])
+        except TypeError, e:
+          distance = (data[i] - data[j]) * (data[i] - data[j])
 
-                distances[(i, j)] = distance / 2
+        distances[(i, j)] = distance / 2
 
-    while len(clusters) > 1:
-        
-        # compute nearest neighbours
+  while len(clusters) > 1:
 
-        neighbour = { }
+    # compute nearest neighbours
 
-        for i in range(len(clusters) - 1):
+    neighbour = { }
 
-            dmin = 1.0e100
+    for i in range(len(clusters) - 1):
 
-            for j in range(i + 1, len(clusters)):
-                if distances[(indices[i], indices[j])] >= dmin:
-                    continue
-                dmin = distances[(indices[i], indices[j])]
-                neighbour[i] = dmin, j
+      dmin = 1.0e100
 
-        # find nearest of near neighbours, i.e. the target to merge the source
-        # into
+      for j in range(i + 1, len(clusters)):
+        if distances[(indices[i], indices[j])] >= dmin:
+          continue
+        dmin = distances[(indices[i], indices[j])]
+        neighbour[i] = dmin, j
 
-        dmin = 1.0e100
-        target = None
-        source = None
+    # find nearest of near neighbours, i.e. the target to merge the source
+    # into
 
-        for i in range(len(clusters) - 1):
-            if neighbour[i][0] >= dmin:
-                continue
+    dmin = 1.0e100
+    target = None
+    source = None
 
-            dmin = neighbour[i][0]
-            target = i
-            source = neighbour[i][1]
+    for i in range(len(clusters) - 1):
+      if neighbour[i][0] >= dmin:
+        continue
 
-        # perform the merge
+      dmin = neighbour[i][0]
+      target = i
+      source = neighbour[i][1]
 
-        source_cluster = clusters.pop(source)
-        source_index = indices.pop(source)
-        history.append((copy.deepcopy(clusters[target]), source_cluster, dmin))
+    # perform the merge
 
-        # update distances
+    source_cluster = clusters.pop(source)
+    source_index = indices.pop(source)
+    history.append((copy.deepcopy(clusters[target]), source_cluster, dmin))
 
-        for i in range(len(clusters)):
-            if i == target:
-                continue
-            
-            n_tot = len(clusters[target]) + len(source_cluster) + \
-                len(clusters[i])
+    # update distances
 
-            if indices[i] < indices[target]:
-                d_target = distances[(indices[i], indices[target])]
-            else:
-                d_target = distances[(indices[target], indices[i])]
+    for i in range(len(clusters)):
+      if i == target:
+        continue
 
-            if indices[i] < source_index:
-                d_source = distances[(indices[i], source_index)]
-            else:
-                d_source = distances[(source_index, indices[i])]
+      n_tot = len(clusters[target]) + len(source_cluster) + \
+          len(clusters[i])
 
-            index_low, index_high = min(indices[i], indices[target]), \
-                max(indices[i], indices[target])
+      if indices[i] < indices[target]:
+        d_target = distances[(indices[i], indices[target])]
+      else:
+        d_target = distances[(indices[target], indices[i])]
 
-            distances[(index_low, index_high)] = (
-                (len(clusters[i]) + len(clusters[target])) * d_target +
-                (len(clusters[i]) + len(source_cluster)) * d_source -
-                len(clusters[i]) * dmin) / float(n_tot)
+      if indices[i] < source_index:
+        d_source = distances[(indices[i], source_index)]
+      else:
+        d_source = distances[(source_index, indices[i])]
 
-        # delete the source related distances
+      index_low, index_high = min(indices[i], indices[target]), \
+          max(indices[i], indices[target])
 
-        for index in sorted(distances):
-            if source_index in index:
-                del(distances[index])
+      distances[(index_low, index_high)] = (
+          (len(clusters[i]) + len(clusters[target])) * d_target +
+          (len(clusters[i]) + len(source_cluster)) * d_source -
+          len(clusters[i]) * dmin) / float(n_tot)
 
-        clusters[target] += source_cluster
+    # delete the source related distances
 
-    return history
+    for index in sorted(distances):
+      if source_index in index:
+        del(distances[index])
+
+    clusters[target] += source_cluster
+
+  return history
 
 if __name__ == '__main__':
 
-    data = [2.0, 6.1, 5.1, 6.2, 2.2, 2.3, 2.4, 0.1, 0.2, 0.3]
+  data = [2.0, 6.1, 5.1, 6.2, 2.2, 2.3, 2.4, 0.1, 0.2, 0.3]
 
-    history = ward_cluster(data)
+  history = ward_cluster(data)
 
-    for i, j, d in history:
-        print '%.2f' % d, [data[_i] for _i in i], [data[_j] for _j in j]
+  for i, j, d in history:
+    print '%.2f' % d, [data[_i] for _i in i], [data[_j] for _j in j]
